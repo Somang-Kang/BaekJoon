bc42b82150ac53b8c045ca63fd291308726b9d59
somang <somang@nsl.inha.ac.kr>
2021-09-02 14:26:51 +0900

    test3

--- 1969/1969_solving.cpp
+++ 1969/1969_solving.cpp
@@ -2,8 +2,22 @@
 // Created by 강소망 on 2021/09/01.
 //
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
-
+vector<vector<string>>Vec2D;
+vector<string> v1;
+int ascii[90];
 int main(){
+    int textsize,nums;
+    cin>>nums>>textsize;
+    while(nums--){
+        string str;
+        cin>>str;
+        v1.push_back(str);
+    }
 
+    for(int i=0;i<v1.size();i++){
+        cout<<v1[i][i];
+    }
 }
